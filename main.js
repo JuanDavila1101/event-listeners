2a171a6ef5d26de87715118a51aa2900f3e0f9b7
JuanDavila1101 <tatojuan713@hotmail.com>
2021-01-23 10:57:32 -0600

    this is selectors

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("event-listeners");
+// console.log("DOM");
+
+const buttonAction = document.getElementById('main-btn');
+console.log(buttonAction);
+
+const buttonActionQuery = document.querySelector('main-btn');
+console.log(buttonActionQuery);
+
+
+const
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// END
