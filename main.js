0b72c0e4fee02b4769f8e0b0f8433b264d9e9734
JuanDavila1101 <tatojuan713@hotmail.com>
2021-01-23 11:08:46 -0600

    this is callback examples

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("event-listeners");
+console.log("callbacks");
+
+function myFunc1(name) {
+
+    return `hello ${name}!`;
+}
+
+function myFunc2(nicname) {
+
+    return `call me ${nicname}!`;
+}
+
+function myFunc3(name, cb) {
+
+    return cb(name);
+}
+
+console.log(myFunc3('Juan', myFunc1));
+console.log(myFunc3('Tato', myFunc2));
+
+
+
+//END
