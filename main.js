1aee2c69fcfa2e6157ca034ef80e2883cccebaf0
JuanDavila1101 <tatojuan713@hotmail.com>
2021-01-28 11:58:45 -0600

    resetting the main branch to work on planning before the group work 1/28/2021

--- main.js
+++ main.js
@@ -1,65 +1,11 @@
- console.log("event-listeners3");
+ console.log("event-listeners Try Again");
 
 
- // const btnClickMe = document.querySelector('#main-btn');
-
- // const btnClickMeToo = document.querySelector('#main-btnToo');
- // const container = document.querySelector('#container');
 
 
- // btnClickMe.addEventListener('click', () => {
- // console.log('You clicked me!');
- // });
-
- // btnClickMe.addEventListener('click', (e) => {
- //   console.log('You clicked me!');
- //   });
-
- // btnClickMe.addEventListener('click', (event) => {
- //     console.log('You clicked me!');
- //     console.log(event);
- // });
-
- // btnClickMe.addEventListener('click', (event) => {
- //     console.log(`You clicked me! ${event.target.id}`);
- //     console.log(event.altKey);
- // });
 
 
- // //callback
- // const whatIsTheId = (event) => {
- //     console.log(`You clicked ${event.target.id}`)
- // }
 
- console.log("event-listeners2");
-
-
- const btnClickMe = document.querySelector('#main-btn');
-
- const btnClickMeToo = document.querySelector('#main-btnToo');
- const container = document.querySelector('#container');
-
- //callback
- const whatIsTheId = (event) => {
-     console.log(`You clicked ${event.target.id}`);
-
-     if (event.target.id === 'main-btn') {
-         container.innerHTML = 'You clicked the main button ';
-     } else {
-         container.innerHTML = 'You clicked the other button ';
-     }
-
- }
-
- btnClickMe.addEventListener('click', whatIsTheId);
-
- btnClickMeToo.addEventListener('click', whatIsTheId);
-
-
- // Const whatIsTheId = e => {
- //   return `The element ID is ${e.target.id}!`;
- //  }
- // //  Button.addEventListener('click', whatIsTheId);
 
 
  // END
